45dd39662ee3cc65a57465f6fb9872e2d803b1df
Jose Alfredo Chavarria <46409841+Jacm1607@users.noreply.github.com>
2024-03-06 23:39:15 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,93 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const JsonFileAdapter = require('@bot-whatsapp/database/json')
+siguiente
+const flowSecundario = addKeyword(['2', '']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+
+const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+    [
+        '🙌 Aquí encontras un ejemplo rapido',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowGracias = addKeyword(['gracias', 'grac', '2']).addAnswer(
+    [
+        '🚀 Puedes aportar tu granito de arena a este proyecto',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+    .addAnswer('🙌 Hola en que te puedo ayudar?...')
+    .addAnswer(
+        [
+            'te comparto los siguientes links de interes sobre el proyecto',
+            '👉 escribe *doc* para ver la documentación',
+            '👉 escribe *gracias*  para ver la lista de videos',
+            '👉 escribe *discord* unirte al discord',
+        ],
+        null,
+        null,
+        [flowDocs, flowGracias, flowTuto, flowDiscord]
+    )
+
+const Bienvenido = addKeyword(['ola'])
+.addAnswer(
+    [
+        'hola en que te puedo ayudar',
+        'https://www.youtube.com'
+    ], 
+    null,
+    null, 
+    [
+        flowGracias
+    ]
+)
+
+
+const main = async () => {
+    const adapterDB = new JsonFileAdapter()
+    const adapterFlow = createFlow([Bienvenido])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
